07b44719ad4c4bd497b6a8c9073dd4b5173f950e
Prachi <you@example.com>
2022-02-02 10:01:15 +0530

    initial commit

---
+++ first.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+print("dsgvbxvcc vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv hjjjjjjjjjjjjjjjjjjjjjjjjjjjj kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
+
+print('The location of python interpreter is:"C:\\new folder\\temp\\Python:\\"')
+
+#print('"In Python,we use","'\t' to give tab. But,if you want to print '\t' then you have to type '\\t' "')
