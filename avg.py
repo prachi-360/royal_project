a95a983a01220b893a1ccde2519527ddb471fb78
prachi-360 <77821825+prachi-360@users.noreply.github.com>
2022-02-04 09:36:54 +0530

    Update avg.py

--- avg.py
+++ avg.py
@@ -1,7 +1,14 @@
-print("Hello World")
+print("Enter five integers:")
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
 
-print("dsgvbxvcc vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv hjjjjjjjjjjjjjjjjjjjjjjjjjjjj kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk")
-
-print('The location of python interpreter is:"C:\\new folder\\temp\\Python:\\"')
-
-#print('"In Python,we use","'\t' to give tab. But,if you want to print '\t' then you have to type '\\t' "')
+sum=a+b+c+d+e
+avg=sum/5
+#print("The average of",a,b,c,d,  "and" ,e,"is:",avg)
+#print()
+# print("The average of", a, "\b,", b, "\b,", c, "\b,", d, "and", e, "is:", avg)
+# f-string
+print(f"The average of {a}, {b}, {c}, {d} and {e} is: {avg}")
